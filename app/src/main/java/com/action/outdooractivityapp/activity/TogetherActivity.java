fe3d24e02d724a4b740c44554518619c62810633
SongMinHo <thdalsehf@naver.com>
2020-05-28 16:16:27 +0900

    함께해요 xml파일 생성, 매니페스트 등록

--- app/src/main/java/com/action/outdooractivityapp/activity/TogetherActivity.java
+++ app/src/main/java/com/action/outdooractivityapp/activity/TogetherActivity.java
@@ -20,7 +20,7 @@ public class TogetherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_together);
 
         initializeView();
 
